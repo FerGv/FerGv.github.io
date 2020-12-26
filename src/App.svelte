23788263b8b226cfe1b6a1f7067249fdf9a03e30
Fer Gv <fergv@Fers-MacBook-Pro.local>
2020-12-26 17:20:36 -0600

    [General] Create base svelte project 

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Components
+  import About from './About.svelte';
+  import Footer from './Footer.svelte';
+  import Header from './Header.svelte';
+  import Projects from './Projects.svelte';
+  import Skills from './Skills.svelte';
+</script>
+
+<main>
+  <Header />
+  <About />
+  <Projects />
+  <Skills />
+  <Footer />
+</main>
