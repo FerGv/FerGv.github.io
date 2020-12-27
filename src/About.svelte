6afe011b60e5aa12b24476cf03ff4f9506b0a4dd
Fer Gv <fergv@Fers-MacBook-Pro.local>
2020-12-26 19:02:03 -0600

    [About] Add photo and description

--- src/About.svelte
+++ src/About.svelte
@@ -1,1 +1,36 @@
-<h1>About</h1>
+<style>
+</style>
+
+<header class="flex flex-col md:flex-row justify-center items-center py-5">
+  <img
+    src="https://avatars2.githubusercontent.com/u/10598981?s=460&u=834eb543ae75b8fe7d4b332672870741f0f1d036&v=4"
+    alt="Me"
+    class="rounded-full w-1/3 md:w-1/4" />
+
+  <section class="px-5 mt-4 md:mt-0">
+    <p>Hola, mi nombre es Fernando.</p>
+
+    <br />
+
+    <p>
+      Soy egresado de la Lic. en Ciencias de la Informática y me encanta el
+      desarrollo de aplicaciones (web, móvil, etc).
+    </p>
+
+    <br />
+
+    <p>
+      Te dejo mi contacto por si gustas charlar o contar un buen chiste. Debajo
+      podrás encontrar algunos de mis proyectos y otras cosas interesantes sobre
+      mí.
+    </p>
+
+    <br />
+
+    <p>Saludos :D</p>
+
+    <p class="text-green-500 text-center"><i class="fas fa-phone fa-fw" /> (+52) 55 3759 2020</p>
+
+    <p class="text-green-500 text-center"><i class="fas fa-envelope fa-fw" /> jfer.garciav@gmail.com</p>
+  </section>
+</header>
