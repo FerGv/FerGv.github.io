8ea298fe5307c9f899c9c29433b21821f316cf06
Fer Gv <fergv@Fers-MacBook-Pro.local>
2020-12-26 18:34:57 -0600

    [Header] Base style

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,1 +1,24 @@
-<h1>Header</h1>
+<style>
+  header {
+    background-image: url("/img/header-bg.jpg");
+    background-position: center;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .name {
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+</style>
+
+<header
+  class="bg-gray-700 text-center min-h-screen flex flex-col justify-center">
+  <section class="name py-10">
+    <p class="text-5xl">Fernando García</p>
+
+    <hr class="w-1/2 m-auto my-3 border-2 border-black" />
+
+    <p class="text-2xl">Desarrollador</p>
+  </section>
+</header>
