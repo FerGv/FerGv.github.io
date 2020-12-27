f11bcfe75ed087c394a3e1785109fd2da5357180
Fer Gv <fergv@Fers-MacBook-Pro.local>
2020-12-26 18:09:05 -0600

    [Footer] Add icons and base style

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,1 +1,19 @@
-<h1>Footer</h1>
+<script>
+  const year = new Date().getFullYear();
+  const socialIcons = ['github', 'linkedin'];
+</script>
+
+<footer class="bg-gray-900 text-white text-center py-5">
+  <section>
+    {#each socialIcons as icon}
+      <i class="fab fa-{icon} fa-fw text-3xl transform hover:scale-150" />
+    {/each}
+  </section>
+
+  <section>
+    <i class="fas fa-code fa-fw text-blue-500" />
+    con
+    <i class="fas fa-heart fa-fw text-red-500" />
+    por Fernando García en {year} &copy;
+  </section>
+</footer>
