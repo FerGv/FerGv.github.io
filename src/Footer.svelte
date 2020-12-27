c8ce7ddf663df2131ab78fca8fdfac16f30504bb
Fer Gv <fergv@Fers-MacBook-Pro.local>
2020-12-26 21:26:11 -0600

    [Footer] Add links

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,12 +1,23 @@
 <script>
   const year = new Date().getFullYear();
-  const socialIcons = ['github', 'linkedin'];
+  const socialIcons = [
+    {
+      icon: 'github',
+      link: 'https://github.com/FerGv',
+    },
+    {
+      icon: 'linkedin',
+      link: 'https://www.linkedin.com/in/jose-fernando-garcia-vazquez',
+    },
+  ];
 </script>
 
 <footer class="bg-gray-900 text-white text-center py-5">
   <section>
-    {#each socialIcons as icon}
-      <i class="fab fa-{icon} fa-fw text-3xl transform hover:scale-150" />
+    {#each socialIcons as socialIcon}
+      <a href={socialIcon.link} target="_blank">
+        <i class="fab fa-{socialIcon.icon} fa-fw text-3xl transform hover:scale-150" />
+      </a>
     {/each}
   </section>
 
